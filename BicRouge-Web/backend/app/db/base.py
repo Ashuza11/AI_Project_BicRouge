3d754fe948312adaac78bb0368729fef62f291b5
Girish Sonune <sonunegirish@gmail.com>
2024-10-31 00:57:27 +0530

    Pushing Flutter code

--- BicRouge-Web/backend/app/db/base.py
+++
@@ -1,2 +0,0 @@
-from models.user_models import *
-from models.operation_models import *
--- BicRouge-Web/backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user_models import Institution, Teacher, Student
-from .operation_models import (
-    Course,
-    Assignment,
-    Question,
-    Enrollment,
-    StudentResponse,
-    Feedback,
-)
